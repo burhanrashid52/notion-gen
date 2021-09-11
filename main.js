564cad52f7f4f90aa74f4bdfd66dc1cfb46ba514
Burhanuddin Rashid <burhanrashid5253@gmail.com>
2021-09-11 09:42:52 +0530

    Generates date utlity

---
+++ main.js
@@ -0,0 +1,90 @@
+const {Client} = require("@notionhq/client")
+// Initializing a client
+let notionSecretToken = '***REMOVED***';
+const notion = new Client({
+    auth: notionSecretToken,
+});
+
+const generateDatesDatabaseId = "***REMOVED***";
+
+async function main() {
+    const args = process.argv.slice(2);
+    const startDate = new Date(args[0]);
+    const endDate = new Date(args[1]);
+    const datesRange = getDaysArray(startDate, endDate);
+    for (let itemDate of datesRange) {
+        console.log("Creating page for " + itemDate)
+        await createPage(itemDate);
+    }
+    return "Done";
+}
+
+function createPage(givenDate) {
+    return notion.pages.create({
+        parent: {
+            database_id: generateDatesDatabaseId,
+        },
+        icon: {
+            type: "emoji",
+            emoji: "📙"
+        },
+        properties: {
+            Name: {
+                title: [
+                    {
+                        text: {
+                            content: getPageName(givenDate),
+                        },
+                    },
+                ],
+            },
+            Date: {
+                date: {
+                    start: formatDateForNotion(givenDate),
+                }
+            },
+        },
+    });
+}
+
+const getDaysArray = function (start, end) {
+    let arr = [];
+    let dt = new Date(start);
+    for (; dt <= end; dt.setDate(dt.getDate() + 1)) {
+        arr.push(new Date(dt));
+    }
+    return arr;
+};
+
+const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+];
+
+function getPageName(givenDate) {
+    return monthNames[givenDate.getMonth()] + " " + givenDate.getDate();
+}
+
+function formatDateForNotion(givenDate) {
+    const month = (givenDate.getMonth() + 1).toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+        useGrouping: false
+    })
+    const day = (givenDate.getDate()).toLocaleString('en-US', {
+        minimumIntegerDigits: 2,
+        useGrouping: false
+    })
+    return givenDate.getFullYear() + "-" + month + "-" + day;
+}
+
+main().then(r => console.log(r))
